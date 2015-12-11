82dd4e303c92f47ce0f346e9ad5e25cf7a1fd74a
c-darwin <you@example.com>
2015-12-11 12:29:15 +0300

    upd win build

--- make_exe/build.bat
+++ make_exe/build.bat
@@ -1,1 +1,8 @@
-go get -u github.com/c-darwin/dcoin-go && go install -ldflags "-H windowsgui" github.com/c-darwin/dcoin-go
+go get -u github.com/jteeuwen/go-bindata/... 
+cd %GOPATH/%src/github.com/c-darwin/dcoin-go
+git stash
+go get -u github.com/c-darwin/dcoin-go 
+go-bindata -o="packages/static/static.go" -pkg="static" static/... 
+go install -ldflags "-H windowsgui" github.com/c-darwin/dcoin-go
+mv C:\go-projects\bin\dcoin-go.exe C:\go-projects\bin\dcoin.exe
+pause
