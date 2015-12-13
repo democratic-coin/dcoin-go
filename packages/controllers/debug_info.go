a337e00a618b170c4c2fc3c1f45d0eb71c014696
c-darwin <you@example.com>
2015-12-13 16:35:26 +0300

    add ver

--- packages/controllers/debug_info.go
+++ packages/controllers/debug_info.go
@@ -23,5 +23,13 @@ func (c *Controller) DebugInfo() (string, error) {
 	if err != nil {
 		return "", utils.ErrInfo(err)
 	}
+	nodesBan, err := c.GetAll(`SELECT * FROM nodes_ban`, 20)
+	if err != nil {
+		return "", utils.ErrInfo(err)
+	}
+	jsonNodesBan, err := json.Marshal(nodesBan)
+	if err != nil {
+		return "", utils.ErrInfo(err)
+	}
 	return string(jsonMainLock)+"\n"+string(jsonNodesBan), nil
 }
