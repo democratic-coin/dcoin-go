564148d356ee64aab0e0c9610f30eb133cecec90
c-darwin <you@example.com>
2015-12-25 12:02:51 +0300

    fix osx icon in the dock

---
+++ dcoindarwin.go
@@ -0,0 +1,30 @@
+// +build darwin
+// +build 386 amd64
+
+package main
+
+import (
+	"time"
+)
+/*
+#cgo darwin CFLAGS: -DDARWIN -x objective-c
+#cgo darwin LDFLAGS: -framework Cocoa
+#import <Cocoa/Cocoa.h>
+void show ()
+{
+    [NSAutoreleasePool new];
+    [NSApplication sharedApplication];
+    [NSApp run];
+}
+
+*/
+import "C"
+
+
+func tray() {
+	go C.show();
+}
+
+func enterLoop() {
+	time.Sleep(3600*24*90 * time.Second)
+}
