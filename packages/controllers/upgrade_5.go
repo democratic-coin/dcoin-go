4df4df2d266ac97869dfcb2d454afcc4c20ef294
Your Name <you@example.com>
2016-02-19 13:05:52 -0500

    init cllocation

--- packages/controllers/upgrade_5.go
+++ packages/controllers/upgrade_5.go
@@ -20,23 +20,29 @@ type upgrade5Page struct {
 }
 
 
-func (c *Controller) Upgrade5() (string, error) {
+var (
+	geolocationLat string
+	geolocationLon string
+)
 
-	log.Debug("Upgrade5")
-
-	geolocationLat := ""
-	geolocationLon := ""
-
-	if !utils.Mobile() {
-		if runtime.GOOS == "darwin" {
+func init() {
+	if runtime.GOOS == "darwin" && !utils.IOS(){
 			if coord, err := geolocation.CLLocation(); err == nil {
 				geolocationLat = fmt.Sprintf("%.6f", coord.Latitude)
 				geolocationLon = fmt.Sprintf("%.6f", coord.Longitude)
 				fmt.Printf("darwin lat: %s\nlng: %s", geolocationLat, geolocationLon)
 			}
 
-		} else
-		if coord, err := geolocation.GetLocation(); err == nil {
+		}
+}
+
+func (c *Controller) Upgrade5() (string, error) {
+
+	log.Debug("Upgrade5")
+
+	if !utils.Mobile() {
+
+		if coord, err := geolocation.GetLocation(); err == nil && runtime.GOOS != "darwin" {
 			geolocationLat = fmt.Sprintf("%.6f", coord.Latitude)
 			geolocationLon = fmt.Sprintf("%.6f", coord.Longitude)
 
