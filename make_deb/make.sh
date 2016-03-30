8e0c88d5858e59cef3c40fa6b34472b8ee5968e5
c-darwin <darwin84@yandex.ru>
2016-03-30 21:54:47 +0300

    upd deb/zip creator

--- make_deb/make.sh
+++ make_deb/make.sh
@@ -1,10 +1,12 @@
 #! /bin/bash -e
 ARCH0=""
+ARCH1="386"
 if [ $# -gt 0 ] && [ $1 = "amd64" ]
 then
   ARCH0="64"
+  ARCH1="amd64"
 fi
 ./bindata.sh
 go get -u github.com/democratic-coin/dcoin-go
-GOARCH=$1  CGO_ENABLED=1  go build -o make_deb/dcoin$ARCH0/usr/share/dcoin/dcoin
+GOARCH=$ARCH1  CGO_ENABLED=1  go build -o make_deb/dcoin$ARCH0/usr/share/dcoin/dcoin
 cd make_deb
