3d41cf97b64f11115df8c74c36b5c39118d048e6
c-darwin <darwin84@yandex.ru>
2016-05-08 20:26:36 +0300

    test gate

--- packages/controllers/e_gate_cp.go
+++ packages/controllers/e_gate_cp.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"fmt"
+	//"github.com/democratic-coin/dcoin-go/packages/utils"
+	//"errors"
 )
 
 func (c *Controller) EGateCP() (string, error) {
@@ -20,5 +22,36 @@ func (c *Controller) EGateCP() (string, error) {
 	fmt.Println(c.r.Header.Get("PHP_AUTH_PW"))
 	log.Error("PHP_AUTH_PW %v", c.r.Header.Get("PHP_AUTH_PW"))
 
+	for k, v := range c.r.Header {
+		log.Error("key: %v / value: %v", k, v)
+	}
+
+/*
+	currencyId := 0
+	if c.r.FormValue("currency1") == "BTC" {
+		currencyId = 1002
+	}
+	if currencyId == 0 {
+		return "", errors.New("Incorrect currencyId")
+	}
+
+	amount := utils.StrToFloat64(c.r.FormValue("amount1"))
+	pmId := utils.StrToInt64(c.r.FormValue("txn_id"))
+	// проверим, не зачисляли ли мы уже это платеж
+	existsId, err := c.Single(`SELECT id FROM e_adding_funds_cp WHERE id = ?`, pmId).Int64()
+	if err != nil {
+		return "", utils.ErrInfo(err)
+	}
+	if existsId != 0 {
+		return "", errors.New("Incorrect txn_id")
+	}
+	paymentInfo := c.r.FormValue("item_name")
+
+	txTime := utils.Time()
+	err = EPayment(paymentInfo, currencyId, txTime, amount, pmId, "cp", c.ECommission)
+	if err != nil {
+		return "", utils.ErrInfo(err)
+	}
+*/
 	return ``, nil
 }
