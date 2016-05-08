3a0efca69c4c06caff7f9757ae49bc2bf19758ca
c-darwin <darwin84@yandex.ru>
2016-05-08 19:54:01 +0300

    testing cp gate

--- packages/controllers/e_gate_cp.go
+++ packages/controllers/e_gate_cp.go
@@ -11,5 +11,14 @@ func (c *Controller) EGateCP() (string, error) {
 	fmt.Println(c.r.Form)
 	log.Error("EGateCP %v", c.r.Form)
 
+	fmt.Println(c.r.Header.Get("HTTP_HMAC"))
+	log.Error("HTTP_HMAC %v", c.r.Header.Get("HTTP_HMAC"))
+
+	fmt.Println(c.r.Header.Get("PHP_AUTH_USER"))
+	log.Error("PHP_AUTH_USER %v", c.r.Header.Get("PHP_AUTH_USER"))
+
+	fmt.Println(c.r.Header.Get("PHP_AUTH_PW"))
+	log.Error("PHP_AUTH_PW %v", c.r.Header.Get("PHP_AUTH_PW"))
+
 	return ``, nil
 }
