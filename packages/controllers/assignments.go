9ee9f26a188e02edbedaa795c00750c4ac7b26ea
c-darwin <darwin84@yandex.ru>
2016-05-11 20:43:15 +0300

    added loop assignments

--- packages/controllers/assignments.go
+++ packages/controllers/assignments.go
@@ -116,14 +116,14 @@ func (c *Controller) Assignments() (string, error) {
 	}
 
 	// модерация акков в соц. сетях
-	mySnType, err := c.Single("SELECT sn_type FROM users WHERE user_id = ?", c.SessUserId).String()
+	/*mySnType, err := c.Single("SELECT sn_type FROM users WHERE user_id = ?", c.SessUserId).String()
 	if err != nil {
 		return "", utils.ErrInfo(err)
-	}
+	}*/
 	addSql = ` AND sn_url_id != ''`
-	if c.SessUserId!=1 && len(mySnType)>0 {
+	/*if c.SessUserId!=1 && len(mySnType)>0 {
 		addSql = ` AND sn_type = "`+mySnType+`"`
-	}
+	}*/
 	num, err = c.Single(`SELECT count(user_id) FROM users WHERE status  =  'user'` + addSql +
 				` AND user_id NOT IN ( SELECT id FROM `+c.MyPrefix+`my_tasks WHERE type=? AND time > ?)`, `sn`,  utils.Time()-consts.ASSIGN_TIME ).Int64()
 	if err != nil {
@@ -134,11 +134,13 @@ func (c *Controller) Assignments() (string, error) {
 	}
 
 	log.Debug("randArr %v", randArr)
+	fmt.Println("randArr", randArr)
 
 	var AssignType int64
 	if len(randArr) > 0 {
 		AssignType = randArr[utils.RandInt(0, len(randArr))]
 	}
+	fmt.Println("AssignType", AssignType)
 
 	cloneHosts := make(map[int64][]string)
 	var photoHosts []string
@@ -371,16 +373,20 @@ func (c *Controller) Assignments() (string, error) {
 
 	case 3:
 
-		mySnType, err := c.Single("SELECT sn_type FROM users WHERE user_id = ?", c.SessUserId).String()
+		/*mySnType, err := c.Single("SELECT sn_type FROM users WHERE user_id = ?", c.SessUserId).String()
 		if err != nil {
 			return "", utils.ErrInfo(err)
-		}
+		}*/
 		addSql = ` AND sn_url_id != ""`
-		if c.SessUserId!=1 && len(mySnType)>0 {
+		/*if c.SessUserId!=1 && len(mySnType)>0 {
 			addSql = ` AND sn_type = "`+mySnType+`"`
+		}*/
+		orderBy := " ORDER BY RAND() "
+		if c.ConfigIni["db_type"] == "sqlite" {
+			orderBy = "ORDER BY RANDOM()"
 		}
 		usersSN, err := c.OneRow(`SELECT user_id, sn_type, sn_url_id FROM users WHERE status  =  'user'` + addSql +
-							` AND user_id NOT IN ( SELECT id FROM `+c.MyPrefix+`my_tasks WHERE type = ? AND time > ?)`, `sn`,  utils.Time()-consts.ASSIGN_TIME ).String()
+							` AND user_id NOT IN ( SELECT id FROM `+c.MyPrefix+`my_tasks WHERE type = ? AND time > ?) `+orderBy+` LIMIT 1`, `sn`,  utils.Time()-consts.ASSIGN_TIME ).String()
 		if err != nil {
 			return "", utils.ErrInfo(err)
 		}
