f336dd66d2887830b5153df45956171ad907581b
Yuriy <yuriy.lomakin@gmail.com>
2016-06-22 16:51:17 +0300

    update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -18,17 +18,6 @@ function ResizeHeader(){
 $(document).ready(function(){
 	'use strict';
 	
-	ResizeHeader();
-	
-	$("header .menu").on('click', function(){
-		alert("");
-		if ($("header").hasClass("on")) {
-			$("header").removeClass("on");
-		} else {
-			$("header").addClass("on");
-		}
-	});
-	
 	jQuery.os = { name: (/(win|mac|linux|sunos|solaris|iphone|ipad)/.exec(navigator.platform.toLowerCase()) || [u])[0].replace('sunos', 'solaris') };
 	if (jQuery.os.name === "mac" || jQuery.os.name === "iphone" || jQuery.os.name === "ipad") {
 		$("body").addClass("macfix");
