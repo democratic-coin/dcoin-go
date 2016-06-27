d05ba8776c17ba6a796dd6dfe0087f4238cf33d9
Alexey Krivonogov <akrivonogov@gmail.com>
2016-06-27 18:24:48 +0500

    Updated exchange support - admin interface

--- packages/controllers/e_ticket.go
+++ packages/controllers/e_ticket.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"errors"
 	"github.com/democratic-coin/dcoin-go/packages/utils"
 //	"strings"
 	"html"
@@ -12,13 +13,33 @@ func (c *Controller) ETicket() (string, error) {
 	if c.SessUserId == 0 {
 		return c.Lang["sign_up_please"], nil
 	}
+
 	c.r.ParseForm()
+	userId := c.SessUserId
 	subject := html.EscapeString(c.r.FormValue("subject"))
 	topic := html.EscapeString(c.r.FormValue("topic"))
 	idroot := utils.StrToInt64(c.r.FormValue("idroot"))
+	userid := utils.StrToInt64(c.r.FormValue("userid"))
+	status := 1   // not read
+
+	if userid > 0 && (!c.NodeAdmin || c.SessRestricted != 0) {
+		return "", utils.ErrInfo(errors.New("Permission denied"))
+	}
+	if userid > 0 {
+		exist, err := c.Single(`select id from e_users where id=?`, userid).Int64()
+		if exist == 0 || err != nil {
+			return "Unknown User ID", nil
+		}
+		if idroot == 0 {
+			status |= 2  // From admin
+			userId = userid
+		} else {
+			userId = 0
+		}
+	}
 	
 	err := c.ExecSql(`insert into e_tickets (user_id, subject, topic, idroot, time, status, uptime) 
-	                 values(?,?,?,?,datetime('now'), 1,datetime('now'))`, c.SessUserId, subject, topic, idroot )
+	                 values(?,?,?,?,datetime('now'), ?,datetime('now'))`, userId, subject, topic, idroot, status )
 	if err == nil && idroot>0 {
 		c.ExecSql(`update e_tickets set uptime=datetime('now') where id=?`, idroot )
 	}			
--- packages/controllers/exchange_support.go
+++ packages/controllers/exchange_support.go
@@ -1,35 +1,35 @@
 package controllers
 
 import (
-	"github.com/democratic-coin/dcoin-go/packages/utils"
+	"errors"
 	"strings"
-//	"fmt"
-/*	"encoding/base64"
-	"sort"
-	"time"
-	"math"*/
+	"github.com/democratic-coin/dcoin-go/packages/utils"
+	"fmt"
 )
 
-type eMySupportPage struct {
-	Lang             map[string]string
-	UserId           int64
+type ExchangeSupportPage struct {
+	UserId   int64
+	ToUserId   int64
+	Lang     map[string]string
 	List             []map[string]string
 	Topic            string
 	IdRoot           int64
 }
 
-func (c *Controller) EMySupport() (string, error) {
+func (c *Controller) ExchangeSupport() (string, error) {
+
 	var ( err error
 		topic string
 		list []map[string]string
-		idRoot int64
+		idRoot, toUserId int64
 	)
-	if c.SessUserId == 0 {
-		return `<script language="javascript"> window.location.href = "` + c.EURL + `"</script>If you are not redirected automatically, follow the <a href="` + c.EURL + `">` + c.EURL + `</a>`, nil
+	if !c.NodeAdmin || c.SessRestricted != 0 {
+		return "", utils.ErrInfo(errors.New("Permission denied"))
 	}
+
 	list = make([]map[string]string, 0)
 	if idroot, ok := c.Parameters[`idroot`]; ok {
-		first, err := c.OneRow( `select * from e_tickets where id=? and user_id=?`, idroot, c.SessUserId ).String()
+		first, err := c.OneRow( `select * from e_tickets where id=?`, idroot).String()
 		if err != nil {
 			return "", utils.ErrInfo(first)
 		}
@@ -44,15 +44,16 @@ func (c *Controller) EMySupport() (string, error) {
 			list = append(list, answers... )
 		}
 		latest := list[len(list)-1]
+		toUserId = utils.StrToInt64(list[0][`user_id`])
 		user_id := utils.StrToInt64(latest[`user_id`])
 		status := utils.StrToInt64(latest[`status`])
-		if ( user_id != c.SessUserId || status & 2 != 0 ) && status & 1 != 0 {
+		if user_id > 0 && status & 2 == 0 && status & 1 == 1 {
 			c.ExecSql(`update e_tickets set status = status & ~1 where id=?`, latest[`id`] )
 		}
 	} else {
 		list, err = c.GetAll( `select id, subject, user_id, uptime, status,
 		(select count(id) from e_tickets where idroot=e.id) as count
-		from e_tickets as e where e.idroot=0 and e.user_id=? order by uptime desc`, 20, c.SessUserId )
+		from e_tickets as e where e.idroot=0 order by uptime desc`, 50 )
 
 		if err != nil {
 			return "", utils.ErrInfo(err)
@@ -69,26 +70,27 @@ func (c *Controller) EMySupport() (string, error) {
 				user_id = latest[`user_id`]
 				status = latest[`status`]
 			}
-			if ( user_id != c.SessUserId || status & 2 != 0 ) && status & 1 != 0 {
+			if user_id > 0 && status & 2 == 0 && status & 1 == 1 {
 				list[key][`toread`] = `1`
 			} else {
 				list[key][`toread`] = ``
 			}
+
 		}
-		if user_id == c.SessUserId {
+		if user_id == 0 {
 			list[key][`author`] = ``
 		} else {
-			list[key][`author`] = `admin`
+			list[key][`author`] = fmt.Sprintf( `ID %d`, user_id )
 		}
 	} 
-	TemplateStr, err := makeTemplate("e_my_support", "eMySupport", &eMySupportPage{
-		Lang:             c.Lang,
-		UserId:           c.SessUserId,
+
+	TemplateStr, err := makeTemplate("exchange_support", "exchangeSupport", &ExchangeSupportPage{
+		Lang:     c.Lang,
 		List:             list,
 		Topic:            topic,
 		IdRoot:           idRoot,
-//		Collapse:         collapse,
-	})
+		ToUserId:         toUserId,
+		UserId:   c.SessUserId})
 	if err != nil {
 		return "", utils.ErrInfo(err)
 	}
