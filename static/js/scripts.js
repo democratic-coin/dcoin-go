4b966663780fc553708482c4e81ee43b78b5bcc6
Yuriy <yuriy.lomakin@gmail.com>
2016-06-29 13:32:52 +0300

    update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,12 @@ function ResizeHeader(){
 	'use strict';
 	
 	var w = $("body").width();
-	if (w >= 976 && w < 1500) {
+	if (w >= 992 && w < 1500) {
 		$("header .logo").prependTo($("header .navbar-nav"));
 	} else {
 		$("header .logo").prependTo($("header"));
 	}
-	if (w < 976) {
+	if (w < 992) {
 		$("header .login").insertBefore($("header .navbar-nav ul"));
 	} else {
 		$("header .login").appendTo($(".mainmenu ul"));
@@ -22,3 +22,60 @@ function HideMenu(){
 		$("header").removeClass("on");
 	}
 }
+
+(function($) { 
+   $.fn.touchwipe = function(settings) {
+     var config = {
+    		min_move_x: 50,
+ 			wipeLeft: function() {},
+ 			wipeRight: function() {},
+			preventDefaultEvents: true
+	 };
+     
+     if (settings) $.extend(config, settings);
+ 
+     this.each(function() {
+    	 var startX;
+		 var isMoving = false;
+
+    	 function cancelTouch() {
+    		 this.removeEventListener('touchmove', onTouchMove);
+    		 startX = null;
+    		 isMoving = false;
+    	 }	
+    	 
+    	 function onTouchMove(e) {
+    		 if(config.preventDefaultEvents) {
+    			 //e.preventDefault();
+    		 }
+    		 if(isMoving) {
+	    		 var x = e.touches[0].pageX;
+	    		 var dx = startX - x;
+	    		 if(Math.abs(dx) >= config.min_move_x) {
+	    			cancelTouch();
+	    			if(dx > 0) {
+	    				config.wipeLeft();
+	    			}
+	    			else {
+	    				config.wipeRight();
+	    			}
+	    		 }
+    		 }
+    	 }
+    	 
+    	 function onTouchStart(e)
+    	 {
+    		 if (e.touches.length == 1) {
+    			 startX = e.touches[0].pageX;
+    			 isMoving = true;
+    			 this.addEventListener('touchmove', onTouchMove, false);
+    		 }
+    	 }    	 
+    		 
+		 this.addEventListener('touchstart', onTouchStart, false);
+     });
+ 
+     return this;
+   };
+ 
+ })(jQuery);
