c55ab6597a65aa7c6ccf04e9f4f9d94b48791301
c-darwin <darwin84@yandex.ru>
2016-08-31 12:17:34 +0300

    fixed column name

--- packages/dcparser/new_reduction.go
+++ packages/dcparser/new_reduction.go
@@ -260,7 +260,7 @@ func (p *Parser) NewReduction() error {
 			return p.ErrInfo(err)
 		}
 		//err = p.ExecSql("UPDATE forex_orders SET amount_backup = amount, amount = round(amount*"+utils.Float64ToStr(d)+"+"+utils.Float64ToStr(consts.ROUND_FIX)+", 2) WHERE sell_currency_id = ?", p.TxMaps.Int64["currency_id"])
-		err = p.ExecSql("UPDATE cf_funding SET amount = round(amount*"+utils.Float64ToStr(d)+"+"+utils.Float64ToStr(consts.ROUND_FIX)+", 2) WHERE sell_currency_id = ?", p.TxMaps.Int64["currency_id"])
+		err = p.ExecSql("UPDATE cf_funding SET amount = round(amount*"+utils.Float64ToStr(d)+"+"+utils.Float64ToStr(consts.ROUND_FIX)+", 2) WHERE currency_id = ?", p.TxMaps.Int64["currency_id"])
 		if err != nil {
 			return p.ErrInfo(err)
 		}
